e37cb908174272b0d56948a476d54ea21f23203f
XanderKjoller <76128467+XanderKjoller@users.noreply.github.com>
2025-04-01 11:07:00 +0200

    Revert "crude delete og display af ønsker under given liste"
    This reverts commit 645e4f0fa564ead923830e32772b924c008aa70e.

--- src/main/java/com/example/wishlist/service/BaseService.java
+++ src/main/java/com/example/wishlist/service/BaseService.java
@@ -1,10 +1,7 @@
 package com.example.wishlist.service;
 
 import com.example.wishlist.models.Bruger;
-import com.example.wishlist.models.Onske;
-import com.example.wishlist.models.OnskeListe;
 import com.example.wishlist.repository.BrugerRepository;
-import com.example.wishlist.repository.OnskeListeRepository;
 import org.springframework.stereotype.Service;
 
 
@@ -14,11 +11,8 @@ public class BaseService {
 
     private final BrugerRepository brugerRepository;
 
-    private final OnskeListeRepository onskeListeRepository;
-
-    public BaseService(BrugerRepository brugerRepository, OnskeListeRepository onskeListeRepository, OnskeListeRepository){
+    public BaseService(BrugerRepository brugerRepository){
         this.brugerRepository = brugerRepository;
-        this.onskeListeRepository = onskeListeRepository;
     }
 
 
@@ -28,21 +22,5 @@ public class BaseService {
 
     public boolean logInd(String userName, String password) {
         return brugerRepository.logInd(userName,password);
-    }
-
-
-
-    public Onske addOnske(Onske onske, String listName) {
-        onskeListeRepository.addWishTo(listName, onske);
-        return onske;
-    }
-
-    public OnskeListe searchForList(String listID) {
-        return onskeListeRepository.searchLists(listID);
-    }
-
-    public Onske updOnske(Onske onske, String listID) {
-        return
-                onskeListeRepository.addWishTo(listID,onske);
     }
 }
