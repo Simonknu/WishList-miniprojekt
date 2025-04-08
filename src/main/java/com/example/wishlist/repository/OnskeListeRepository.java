aa8fe65e49e71b23753b7e26178581a2d716f83a
Simonknu <sikn0002@stud.kea.dk>
2025-04-08 09:37:38 +0200

    startig with wishes

--- src/main/java/com/example/wishlist/repository/OnskeListeRepository.java
+++ src/main/java/com/example/wishlist/repository/OnskeListeRepository.java
@@ -1,12 +1,11 @@
 package com.example.wishlist.repository;
 
 
-import com.example.wishlist.models.Bruger;
-import com.example.wishlist.models.Onske;
-import com.example.wishlist.models.OnskeListe;
-import com.example.wishlist.models.OnskeListeRowMapper;
+import com.example.wishlist.models.*;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 
@@ -20,17 +19,46 @@ public class OnskeListeRepository {
 
     private final JdbcTemplate jdbcTemplate;
     private final OnskeListeRowMapper onskeListeRowMapper = new OnskeListeRowMapper();
+    private final OnskeRowMapper onskeRowMapper = new OnskeRowMapper();
 
 
     //----------------------------------CONSTRUCTOR---------------------------------------------------------------
     public OnskeListeRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-//---------------------------------------GETTER METHOD--------------------------------------------------------------
+
+    //---------------------------------------GETTER METHOD--------------------------------------------------------------
     public JdbcTemplate getJdbcTemplate() {
         return jdbcTemplate;
     }
+
+    //---------------------------------ROW MAPPERS-----------------------------------------------
+    public class OnskeListeRowMapper implements RowMapper<OnskeListe> {
+        @Override
+        public OnskeListe mapRow(ResultSet rs, int rowNum) throws SQLException {
+            String name = rs.getString("wishList_name");
+
+            List<Onske> onsker = faOnsker(name);
+
+            // Return the Attraction object with tags
+            return new OnskeListe(name, onsker);
+        }
+    }
+
+    public class OnskeRowMapper implements RowMapper<Onske> {
+
+        @Override
+        public Onske mapRow(ResultSet rs, int rowNum) throws SQLException {
+            String name = rs.getString("wish_name");
+            String description = rs.getString("description");
+            String link = rs.getString("link");
+
+            return new Onske(name, description, link);
+        }
+    }
+
 //------------------------------------------------------------------------------------------
+
     public List<OnskeListe> faOnskeListeFraBruger(String username) {
         String sql = "SELECT w.wishList_name " +
                 "FROM wishLists w " +
@@ -40,6 +68,15 @@ public class OnskeListeRepository {
         return jdbcTemplate.query(sql, onskeListeRowMapper, username);
     }
 
+    public List<Onske> faOnsker(String name) {
+        String sql = "SELECT w.wish_name " +
+                "FROM wishes w " +
+                "JOIN wishLists wL ON w.wishList_id = wL.id" +
+                " WHERE wL.wishList_name = ?";
+
+        return jdbcTemplate.query(sql, onskeRowMapper, name);
+    }
+
     public List<OnskeListe> faAllOnskeListe() {
         String sql = "SELECT w.wishList_name " +
                 "FROM wishlists w";
@@ -47,8 +84,10 @@ public class OnskeListeRepository {
     }
 
 
-    public OnskeListe findOnskeListeMedNavn (String name) {
+    public OnskeListe findOnskeListeMedNavn(String name) {
         String sql = "SELECT w.wishList_name FROM wishLists w WHERE wishList_name = ?";
+
+        List<Onske> onsker = faOnsker(name);
         try {
             return jdbcTemplate.queryForObject(sql, onskeListeRowMapper, name);
         } catch (EmptyResultDataAccessException e) {
@@ -70,9 +109,9 @@ public class OnskeListeRepository {
     }
 
     public void sletOnskeListe(String name) {
-String sql = "DELETE FROM wishLists WHERE wishList_name = ?";
+        String sql = "DELETE FROM wishLists WHERE wishList_name = ?";
 
-jdbcTemplate.update(sql, name);
+        jdbcTemplate.update(sql, name);
     }
 
     public void redigerOnskeListe(String oldName, String newName) {
@@ -82,8 +121,8 @@ jdbcTemplate.update(sql, name);
     }
 
 
-    public boolean gentagetNavn(String name){
-        if (findOnskeListeMedNavn(name) == null){
+    public boolean gentagetNavn(String name) {
+        if (findOnskeListeMedNavn(name) == null) {
             return false;
         }
         return true;
